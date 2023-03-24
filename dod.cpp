89d89143c2076526781418845a099fca98620094
MeLikeyCode <abdullah.aghazadah@gmail.com>
2023-03-24 00:42:24 -0400

    improve effeciency and readability/maintainability. Also add vscode task for building dod.cpp

--- dod.cpp
+++ dod.cpp
@@ -9,15 +9,17 @@
 // when USE_STATIC is defined, its value (which should be a number), is used to determine the number of components a point will have
 
 // By the way, this is how you use msvc compiler to compile this file with USE_STATIC defined to 100:
-//	cl /DUSE_STATIC=100 /EHsc /O2 main.cpp
+//	cl /DUSE_STATIC=100 /EHsc /O2 dod.cpp
 // /Ehsc enables exception handling, /O2 does max optimization
 
 // And here is how you do it with g++
-// g++ -DUSE_STATIC=100 -O2 main.cpp -o main.exe
+// g++ -DUSE_STATIC=100 -O2 dod.cpp -o dod.exe
 
 #ifdef USE_STATIC
 const int NUM_COMPONENTS_STATIC = USE_STATIC; // if USE_STATIC, this is the number of components a point will have
 #endif
+
+const int INNER_REPEAT = 1; // how many times to repeat the inner loop (to make benchmarking more compute bound)
 
 struct Answer {
 	std::vector<double> totals; // totals[0] = sum of all points' first component, totals[1] = sum of all points' second component, etc.
@@ -34,7 +36,7 @@ struct Point
 };
 
 // Calculate component wise sum, given a struct of arrays
-Answer do_soa(std::vector<std::vector<double>> components)
+Answer do_soa(const std::vector<std::vector<double>>& components)
 {
 	int numPoints = components[0].size();
 	int numComponents = components.size();
@@ -43,12 +45,16 @@ Answer do_soa(std::vector<std::vector<double>> components)
 
 	auto start = std::chrono::high_resolution_clock::now();
 	
-	for (size_t i = 0; i < numComponents; i++)
+	for (size_t c = 0; c < numComponents; c++)
 	{
-		for (size_t j = 0; j < numPoints; j++)
+		for (size_t repeat = 0; repeat < INNER_REPEAT; repeat++)
 		{
-			answer.totals[i] += components[i][j];
+			for (size_t p = 0; p < numPoints; p++)
+			{
+				answer.totals[c] += components[c][p];
+			}
 		}
+		
 	}
 
 	auto end = std::chrono::high_resolution_clock::now();
@@ -59,7 +65,7 @@ Answer do_soa(std::vector<std::vector<double>> components)
 }
 
 // Calculate component wise sum, given an array of structs
-Answer do_aos(std::vector<Point> points)
+Answer do_aos(const std::vector<Point>& points)
 {
 	int numComponents = points[0].components.size();
 	int numPoints = points.size();
@@ -68,11 +74,14 @@ Answer do_aos(std::vector<Point> points)
 
 	auto start = std::chrono::high_resolution_clock::now();
 
-	for (size_t i = 0; i < numComponents; i++)
+	for (size_t c = 0; c < numComponents; c++)
 	{
-		for (size_t j = 0; j < numPoints; j++)
+		for (size_t repeat = 0; repeat < INNER_REPEAT; repeat++)
 		{
-			answer.totals[i] += points[j].components[i];
+			for (size_t p = 0; p < numPoints; p++)
+			{
+				answer.totals[c] += points[p].components[c];
+			}
 		}
 	}
 
@@ -125,24 +134,24 @@ int main(int argc, char* argv[])
 	{
 		std::vector<double> values;
 		values.resize(numPoints);
-		for (size_t i = 0; i < numPoints; i++)
+		for (size_t c = 0; c < numPoints; c++)
 		{
-			values[i] = 1;
+			values[c] = 1;
 		}
 		soa.push_back(values);
 	}
 
 	// array of structs
 	std::vector<Point> aos;
-	for (size_t i = 0; i < numPoints; i++)
+	for (size_t c = 0; c < numPoints; c++)
 	{
 		Point v;
 #ifndef USE_STATIC
 		v.components.resize(numComponents);
 #endif
-		for (size_t j = 0; j < numComponents; j++)
+		for (size_t p = 0; p < numComponents; p++)
 		{
-			v.components[j] = 1;
+			v.components[p] = 1;
 		}
 		aos.push_back(v);
 	}
@@ -156,9 +165,9 @@ int main(int argc, char* argv[])
 	std::cout << "aos: " << aosAnswer.durationUs << "us" << std::endl;
 
 	// print soa and aos results side by side (for debugging)
-	//for (size_t i = 0; i < soaAnswer.totals.size(); i++)
+	//for (size_t c = 0; c < soaAnswer.totals.size(); c++)
 	//{
-	//	std::cout << soaAnswer.totals[i] << " " << aosAnswer.totals[i] << std::endl;
+	//	std::cout << soaAnswer.totals[c] << " " << aosAnswer.totals[c] << std::endl;
 	//}
 
 	return 0;
